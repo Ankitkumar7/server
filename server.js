9b1dfaca5192ec015a21baa05bacd8b41634fd40
Ankit kumar <ankit.kumar.cs@outlook.com>
2020-08-08 23:54:21 +0530

    bug

--- server.js
+++ server.js
@@ -4,4 +4,4 @@ const config = require('./config/development')
 
 const port = config.server.port
 const server = http.createServer(app);
-server.listen(process.env.PORT || 8080);
+server.listen(process.env.PORT || 8080, 'http://suv-client.herokuapp.com');
