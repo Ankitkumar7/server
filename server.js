9677801cc1cd3bd3ce6e304036bc507519c47a03
Ankit kumar <ankit.kumar.cs@outlook.com>
2020-08-08 23:32:24 +0530

    changes done

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const http = require('http');
 const app = require('./app')
 const config = require('./config/development')
 const express = require('express');
+
 app.use(express.static('./dist/'));
-app.get('/*', function(req, res) {
+app.get('/', function(req, res) {
     res.sendFile('index.html', {root: 'dist/'}
   );
   });
